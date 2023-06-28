53076d6e196d15289d53ae4498948f2674a89491
Guilherme Amaral <gui.amaral123@outlook.com>
2023-06-28 20:29:05 +0000

    create new note

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.notas = [];
+    }
+
+    criarNota(titulo, texto) {
+        const novaNota = {titulo, texto};
+        this.notas.push(novaNota);
+    }
+
     render() {
         return (
             <section className="conteudo">
-                <FormularioCadastro />
-                <ListaDeNotas />
+                <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+                <ListaDeNotas notas={this.notas}/>
             </section>
         );
     }
